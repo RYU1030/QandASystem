4dd5e531ddf9658037f3e615fd25565a9621030a
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2021-04-14 12:35:29 +0900

    アノテーション修正

--- src/regist.java
+++ src/regist.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class regist
  * @author Ryunosuke Fukuda
- * version 1.0
+ * @version 1.0
  */
 @WebServlet("/regist")
 public class regist extends HttpServlet {
