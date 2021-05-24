42d49af8e4f8703ab1251378c77a33608424bb4b
Ryunosuke Fukuda <ryu30th@kif.biglobe.ne.jp>
2021-05-24 20:58:03 +0900

    サーブレットから回答登録機能を呼び出す。

--- src/answer.java
+++ src/answer.java
@@ -1,11 +1,16 @@
 
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.Answer;
+import model.PostAnswerLogic;
 
 /**
  * Servlet implementation class answer
@@ -13,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/answer")
 public class answer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -34,8 +39,30 @@ public class answer extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+
+		// フォーム送信によってpostされた値を変数に格納
+		int questionId = Integer.parseInt(request.getParameter("question_id"));
+		String answererName = request.getParameter("answerer-name");
+		String answerContent = request.getParameter("answer-content");
+
+		// 必要な情報が全てpostされた場合にのみ、回答登録の処理に移行する
+		if (questionId >= 0 && answererName != "" && answerContent != "") {
+			Answer answer = new Answer(questionId, answererName, answerContent);
+			PostAnswerLogic postAnswerLogic = new PostAnswerLogic();
+			postAnswerLogic.execute(answer);
+
+			// 回答登録完了後、送信前画面にリダイレクトする
+			response.sendRedirect(request.getContextPath() + "/confirm?questionId=" + answer.getQuestionId());
+
+		} else {
+			// 必須項目一つでも未入力の場合は、エラーメッセージを定義の上、質問登録画面にフォワードする
+			request.setAttribute("errorMsgAnswer", "必須項目のいずれか（名前/タイトル）が未入力/未選択です。");
+			request.setAttribute("answererName", answererName);
+			request.setAttribute("answerContent", answerContent);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/confirm?questionId=" + questionId);
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
